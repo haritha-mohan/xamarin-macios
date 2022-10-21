ff73bac098ae2fb365928eecb4c4416b618547cc
Haritha Mohan <harithamohan@microsoft.com>
2022-10-20 18:21:07 -0700

    progress

--- tests/cecil-tests/ApiCapitalizationTest.cs
+++ tests/cecil-tests/ApiCapitalizationTest.cs
@@ -7,13 +7,7 @@ using System.Runtime.Versioning;
 using System.Text;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
-//using ObjCRuntime;
-#if MONOMAC
-using AppKit;
-#else
-//using UIKit;
-#endif
-//using Foundation;
+
 using Mono.Cecil;
 using Xamarin.Tests;
 using Xamarin.Utils;
@@ -24,8 +18,7 @@ namespace Cecil.Tests {
 
 		bool NotObsolete (MethodDefinition mi)
 		{
-			//Console.WriteLine ("Method Name: ", mi.Name);
-			//Console.WriteLine ("Method Public: ", mi.IsPublic);
+
 			if (mi == null)
 				return false;
 
@@ -43,54 +36,78 @@ namespace Cecil.Tests {
 		[TestCaseSource (typeof (Helper), nameof (Helper.PlatformAssemblies))]
 		[TestCaseSource (typeof (Helper), nameof (Helper.NetPlatformAssemblies))]
 		[Test]
-		public void CapitalizationTest (string assemblyPath)
+		public void PropertiesCapitalizationTest (string assemblyPath)
+		{
+			Func<TypeDefinition, IEnumerable <object>> selectLambda = (type) => {
+				var typeName = type.Name;
+				var c = type.Properties.Where (p => p.GetMethod?.IsPublic == true || p.SetMethod?.IsPublic == true).Where (p=>!(Char.IsUpper (p.Name [0]))).Select (p => new { typeName, p.Name });
+				return c;
+			};
+			CapitalizationTest (assemblyPath, selectLambda);
+		}
+
+		[TestCaseSource (typeof (Helper), nameof (Helper.PlatformAssemblies))]
+		[TestCaseSource (typeof (Helper), nameof (Helper.NetPlatformAssemblies))]
+		[Test]
+		public void MethodsCapitalizationTest (string assemblyPath)
+		{
+			Func<TypeDefinition, IEnumerable<object>> selectLambda = (type) => {
+				var typeName = type.Name;
+				var c = from m in type.Methods
+						where m.IsPublic && NotObsolete (m) && !(Char.IsUpper (m.Name [0]))
+						select new { typeName, m.Name }; 
+				return c;
+			};
+			CapitalizationTest (assemblyPath, selectLambda);
+		}
+
+		[TestCaseSource (typeof (Helper), nameof (Helper.PlatformAssemblies))]
+		[TestCaseSource (typeof (Helper), nameof (Helper.NetPlatformAssemblies))]
+		[Test]
+		public void EventsCapitalizationTest (string assemblyPath)
+		{
+			Func<TypeDefinition, IEnumerable<object>> selectLambda = (type) => {
+				var typeName = type.Name;
+				var c = from e in type.Events 
+						where !(Char.IsUpper (e.Name [0]))
+						select new { typeName, e.Name};
+				return c;
+			};
+			CapitalizationTest (assemblyPath, selectLambda);
+		}
+
+		[TestCaseSource (typeof (Helper), nameof (Helper.PlatformAssemblies))]
+		[TestCaseSource (typeof (Helper), nameof (Helper.NetPlatformAssemblies))]
+		[Test]
+		public void FieldsCapitalizationTest (string assemblyPath)
+		{
+			Func<TypeDefinition, IEnumerable<object>> selectLambda = (type) => {
+				var typeName = type.Name;
+				var c = from f in type.Fields
+						where f.IsPublic && !(Char.IsUpper (f.Name [0]))
+						select new {typeName, f.Name};
+				return c;
+			};
+			CapitalizationTest (assemblyPath, selectLambda);
+		}
+
+		public void CapitalizationTest (string assemblyPath, Func<TypeDefinition, IEnumerable<object>> selectLambda)
 		{
 			var assembly = Helper.GetAssembly (assemblyPath);
-			if (assembly == null) {
-				Assert.Ignore ("{assemblyPath} could not be found (might be disabled in build)");
-				return; 
+			if (assembly is null) {
+				Assert.Ignore ($"{assemblyPath} could not be found (might be disabled in build)");
+				return;
 			}
 
-			foreach (var type in assembly.MainModule.Types) {
-
-				var errMethods =
-					(from m in type.Methods
-					where m.IsPublic && NotObsolete (m) && !(Char.IsUpper (m.Name [0]))
-					select m).ToArray();
-
-				var errProperties =
-					from p in type.Properties
-					where p.GetMethod.IsPublic || p.SetMethod.IsPublic
-					where !(Char.IsUpper (p.Name [0]))
-					select p;
-
-				var errEvents =
-					from e in type.Events //how to check if event is public
-					where !(Char.IsUpper (e.Name [0]))
-					select e;
-
-				var errFields =
-					from f in type.Fields
-					where f.IsPublic && !(Char.IsUpper (f.Name [0]))
-					select f;
-
-
-				//Console.WriteLine ("methods are errs", errMethods);
-
-				if (errMethods is not null) {
-					foreach (MethodDefinition m in errMethods) {
-						Assert.Fail ("This method is incorrectly capitalized: ", $"{m.Name} is name");
+			// Walk every class/struct/enum/property/method/enum value/pinvoke/event
+			foreach (var module in assembly.Modules) {
+				foreach (var type in module.Types) {
+					var err = selectLambda (type);
+					if (err is not null) {
+						Assert.AreEqual (0, err.Count (), $"Capitalization Issues Found: {string.Join (";", err)}");
 					}
+
 				}
-
-				if (errProperties is not null) {
-					foreach (PropertyDefinition p in errProperties) {
-						Assert.Fail ("This property is incorrectly capitalized: ", $"{p.Name} is name");
-					}
-				}
-
-				Assert.Pass ("test pass");
-
 			}
 
 		}
