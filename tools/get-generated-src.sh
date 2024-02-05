3d608663c83b3c8c0a220fdea3c38e5de8f41af1
Haritha Mohan <harithamohan@microsoft.com>
2024-02-05 13:48:31 -0800

    no build dir for SL, move embed later

--- tools/get-generated-src.sh
+++ tools/get-generated-src.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 
 directory_path=$1
-file_list=$(find "$directory_path" -type f -printf "%f;")
-file_list="${file_list%;}"
-echo $file_list
+find $(realpath $directory_path) -type f | tr '\n' ';'
