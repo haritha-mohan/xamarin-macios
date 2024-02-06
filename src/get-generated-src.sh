d762c8af0690f540c14a4f2a6f76c73b0811aa55
Haritha Mohan <harithamohan@microsoft.com>
2024-02-05 20:20:27 -0800

    really good script w semicolon but arg list too long

--- src/get-generated-src.sh
+++ src/get-generated-src.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
 
-directory_path=$1
-find $(realpath $directory_path) -type f | tr '\n' ';'
+directory_path="$1"
+
+# # Use find's -exec to print full paths directly
+FILES=$(find $(realpath "$directory_path") -type f -name "*.g.cs")
+
+# # Join file names with semicolons
+RESULT=$(echo "$FILES" | tr '\n' ';')
+
+# echo "Result: $RESULT"
+echo $RESULT
